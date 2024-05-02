2b033a51c7ee3086572c2d3a692506ce09f5e6c5
Felix Pape <ziemlich@schlechte.info>
2024-05-03 01:48:19 +0200

    Moved credentials to .env / environment. Mastodon adapter can get Auth token

--- LetsGoTroet.go
+++ LetsGoTroet.go
@@ -3,30 +3,50 @@ package main
 import (
 	"LetsGoTroet/irc"
   "LetsGoTroet/app"
+  "LetsGoTroet/mastodon"
 	"log"
+  "os"
   "database/sql"
   _ "github.com/mattn/go-sqlite3"
+  "github.com/joho/godotenv"
 )
 
-const CHANNEL = "#troet"
-const NICK = "Troet2"
 const SQLITE_FILENAME = "messages.db"
 
 func main() {
+  err := godotenv.Load(".env")
+  if err != nil {
+    log.Println("Did not load .env file")
+  }
+
+  // Get variables from environment
+  irchost := os.Getenv("IRC_HOST")
+  channel := os.Getenv("IRC_CHANNEL")
+  nick := os.Getenv("IRC_NICK")
+
+  baseurl := os.Getenv("MASTODON_BASEURL")
+  username := os.Getenv("MASTODON_USERNAME")
+  password := os.Getenv("MASTODON_PASSWORD")
   
+  // Setup database
   db := setupDB(SQLITE_FILENAME)
+
 	// Setup  IRC adapter
-  bot, err := irc.New("irc.hackint.org:6697", NICK, CHANNEL, db)
+  bot, err := irc.New(irchost, nick, channel, db)
 	if err != nil {
     log.Println("IRC Adapter creation failed:", err)
-		return
+	  return
 	}
-  // TODO: Setup Mastodon adapter
-
-
+  // Setup Mastodon adapter
+  mst, err := mastodon.New(baseurl, username, password, db) 
+  if err != nil {
+    log.Println(err)
+    return
+  }
   // Run service
-  service := app.New(bot, nil)
-  service.Run()
+  service := app.New(bot, mst)
+  _ = service
+  //service.Run()
 }
 
 func setupDB(filename string) *sql.DB{
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module LetsGoTroet
 
 go 1.21.7
 
-require github.com/mattn/go-sqlite3 v1.14.22 // indirect
+require (
+	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/mattn/go-sqlite3 v1.14.22 // indirect
+)
